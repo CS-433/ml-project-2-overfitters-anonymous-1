aaf625577e86f7e3100e90bfd11793b1fad2f7a4
Max Tost <m.luke.tost@gmail.com>
2024-12-12 19:34:44 +0100

    Gauthiers modification

--- submit.py
+++ submit.py
@@ -4,6 +4,8 @@ import os
 import numpy as np
 import matplotlib.image as mpimg
 import re
+from PIL import Image
+
 
 foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
 output_name = 'Predictions_U-Net_submission.csv' # Folder / name of the output file
@@ -17,16 +19,19 @@ def patch_to_label(patch):
         return 0
 
 
-def mask_to_submission_strings(image_filename):
-    """Reads a single image and outputs the strings that should go into the submission file"""
+def mask_to_submission_strings(image_filename, desired_shape=(400, 400)):
+    """Reads a single image, resizes it, and outputs the strings for the submission file."""
     img_number = int(re.search(r"\d+", image_filename).group(0))
-    im = mpimg.imread(image_filename)
+    im = Image.open(image_filename).convert("L")  # Convert to grayscale if needed
+    im_resized = im.resize(desired_shape, Image.NEAREST)  # Resize using nearest neighbor interpolation
+    im_resized = np.array(im_resized)  # Convert to NumPy array for processing
+
     patch_size = 16
-    for j in range(0, im.shape[1], patch_size):
-        for i in range(0, im.shape[0], patch_size):
-            patch = im[i:i + patch_size, j:j + patch_size]
+    for j in range(0, im_resized.shape[1], patch_size):
+        for i in range(0, im_resized.shape[0], patch_size):
+            patch = im_resized[i:i + patch_size, j:j + patch_size]
             label = patch_to_label(patch)
-            yield("{:03d}_{}_{},{}".format(img_number, j, i, label))
+            yield("{:03d}{}{},{}".format(img_number,j,i,label))
 
 
 def masks_to_submission(submission_filename, *image_filenames):
